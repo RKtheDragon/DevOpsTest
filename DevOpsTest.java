335847102f4a52f1a0f3bbddafd177918796eaaa
RKtheDragon <34155357+RKtheDragon@users.noreply.github.com>
2019-07-08 16:45:53 +0530

    Uploaded on GIT for jenkins linking ver-1.0
    We are going to upload and link Java Files with Jenkins so in next update we will write all the steps for Linking with Jenkins.

---
+++ DevOpsTest.java
@@ -0,0 +1,11 @@
+public class DevOpsTest {
+	
+	public static void main(String [] args) {
+		String myName = "RKtheDragon" ;
+		int myAge =25 ;
+
+		System.out.println("Name is: " + myName +
+							" Age is: " + myAge );
+		return ;
+	}
+}
